afda39202783fa5bf2f48e24d70465967423c4da
Ciarancor <56367012+Ciarancor@users.noreply.github.com>
2020-11-22 22:37:20 +0000

    Update Course.java

--- src/main/java/Course.java
+++ src/main/java/Course.java
@@ -1,24 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
  *
- * @author Ciaran
+ * @author Ciaran Corcoran - 17371636
  */
+
 import java.util.*;
 import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
 
 public class Course {
     private String name;
     private DateTime startDate;
     private DateTime endDate;
-    private String moduleString = "";
-    private String studentString = "";
-    private List<Student> students = new ArrayList<Student>();
-    private List<Modules> modules = new ArrayList<Modules>();
+    private String moduleString = "";  // string array of modules arer added to
+    private String studentString = "";  // string array of students arer added to
+    private List<Student> students = new ArrayList<Student>();  // array of students in this course
+    private List<Modules> modules = new ArrayList<Modules>();   // array modules in this course
 
     public Course(String name, DateTime startDate, DateTime endDate) {
         this.name = name;
@@ -26,18 +23,18 @@ public class Course {
         this.endDate = endDate;
     }
     
-    public void addStudent(Student student){
-        students.add(student);
-        student.addModules(modules);
+    public void addStudent(Student student){  // adds student to this course
+        students.add(student);  // adds student to student array
+        student.addModules(modules);  // gives students its modules
        
         for (int i=0; i<modules.size(); i++){
-            modules.get(i).addStudent(student);
+            modules.get(i).addStudent(student);  // adds new student to each module
         }
     }
     
-    public void addModule(Modules module){
-        modules.add(module);
-        module.addCourse(this);
+    public void addModule(Modules module){  // adds modules to this course
+        modules.add(module);    // adds to courses module array
+        module.addCourse(this);  // adds itself to modules array of courses
     }
 
     public String getName() {
@@ -47,9 +44,17 @@ public class Course {
     public DateTime getStartDate() {
         return startDate;
     }
-
+    
     public DateTime getEndDate() {
         return endDate;
+    }
+    
+    public String getStartDateString() {
+        return startDate.toString(DateTimeFormat.shortDate()); // changes format to only show date
+    }
+
+    public String getEndDateString() {
+        return endDate.toString(DateTimeFormat.shortDate());  // changes format to only show date
     }
 
     public String getModuleString() {
@@ -60,15 +65,13 @@ public class Course {
         return studentString;
     }
     
-    
-    
     public String getModules(){
-        for (int i=0; i<modules.size(); i++){
+        for (int i=0; i<modules.size(); i++){          // converts module to string and returns
             moduleString += modules.get(i).getName()+ " ";
         }
         return moduleString;
     }
-    public String getStudents(){
+    public String getStudents(){    // converts student array to string and returns
         for (int i=0; i<students.size(); i++){
             studentString += students.get(i).getUsername()+ " ";
         }
@@ -77,11 +80,6 @@ public class Course {
 
     @Override
     public String toString() {
-        return "Course{" + "name=" + name + ", startDate=" + startDate + ", endDate=" + endDate + ", students=" + getStudents() + ", modules=" + getModules() + '}';
-    }
-    
-    
-    
-    
-    
+        return "Course{" + "name=" + name + ", startDate=" + getStartDateString() + ", endDate=" + getEndDateString() + ", students=" + getStudents() + ", modules=" + getModules() + '}';
+    }  
 }
