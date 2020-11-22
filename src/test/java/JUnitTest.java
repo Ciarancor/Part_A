00e00ecf9dc371507efeddd34aa33c066bb971f8
Ciarancor <56367012+Ciarancor@users.noreply.github.com>
2020-11-22 22:42:28 +0000

    Update JUnitTest.java

--- src/test/java/JUnitTest.java
+++ src/test/java/JUnitTest.java
@@ -1,16 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ *
+ * @author Ciaran Corcoran - 17371636
  */
 
 import org.joda.time.DateTime;
 import org.junit.*;
 
-/**
- *
- * @author Ciaran
- */
 public class JUnitTest {
     
     @Test
@@ -18,22 +13,15 @@ public class JUnitTest {
         Course ECE = new Course("ECE", new DateTime("2020-09-01"), new DateTime("2020-05-30"));
          
         Modules comps = new Modules("Comps", 387964);
-        Modules elec = new Modules("elec", 182787);
-         
+                
         Student student1 = new Student("Mary", "Jo", "dob", 23, 1234);
-        Student student2 = new Student("Bren", "Fag", "dob", 23, 1234);
-        Student student3 = new Student("Ciaran", "Legend", "dob", 23, 1234);
-         
-        ECE.addModule(comps);
-        ECE.addModule(elec);
-        ECE.addStudent(student1);
-        ECE.addStudent(student2);
-        ECE.addStudent(student3);
-         
+        
+        ECE.addModule(comps);  // adds module t course
+        ECE.addStudent(student1);   // adds student to course
         
         System.out.println(ECE);
-        System.out.println(comps.toString());
-        System.out.println(student1.toString());
+        System.out.println(comps);
+        System.out.println(student1);
          
     }
 
